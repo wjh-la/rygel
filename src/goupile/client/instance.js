6043db090efc30d6019ecae8070b765c44dbe1a8
Niels Martignène <niels.martignene@protonmail.com>
2022-06-30 08:40:14 +0200

    Tolerate dangling record uploads

--- src/goupile/client/instance.js
+++ src/goupile/client/instance.js
@@ -2572,8 +2572,6 @@ function InstanceController() {
                             let err = await net.readError(response);
                             throw new Error(err);
                         }
-
-                        changed = true;
                     }
                 }
 
