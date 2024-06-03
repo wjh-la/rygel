5ea6110513d44d29d23ccec1d47ea0feae624b91
Niels Martignène <niels.martignene@protonmail.com>
2024-06-03 10:26:14 +0200

    Fix flaky test caused by use of non-volatile RBX register

--- src/koffi/test/sync2.c
+++ src/koffi/test/sync2.c
@@ -23,16 +23,17 @@
 
 #ifdef __x86_64__
 
-void DoReturnBool(int cond)
+__attribute__((naked)) void DoReturnBool(int cond)
 {
     // Return an ABI-compliant but weird bool where bits 8 to 31 are set.
     // On x86_64, only the least signifiant byte matters for bools and the rest is explicitly undefined.
 
     __asm__ (
         "cmpl $0, %edi\n"
-        "setne %bl\n"
+        "setne %r10b\n"
         "movl $0xFFFFFFFF, %eax\n"
-        "andb %bl, %al\n"
+        "andb %r10b, %al\n"
+        "ret\n"
     );
 }
 
