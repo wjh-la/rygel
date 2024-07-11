87e9b05d8120a3044b4373dd2f35b66a86fef89d
Niels Martignène <niels.martignene@protonmail.com>
2024-07-11 14:39:57 +0200

    Import musl prebuild support from master branch

--- src/koffi/index.js
+++ src/koffi/index.js
@@ -21,24 +21,9 @@
 
 'use strict';
 
-const util = require('util');
-const fs = require('fs');
-const { get_napi_version, determine_arch } = require('../cnoke/src/tools.js');
-const pkg = require('./package.json');
+const { detect, init } = require('./src/init.js');
 
-if (process.versions.napi == null || process.versions.napi < pkg.cnoke.napi) {
-    let major = parseInt(process.versions.node, 10);
-    let required = get_napi_version(pkg.cnoke.napi, major);
-
-    if (required != null) {
-        throw new Error(`This engine is based on Node ${process.versions.node}, but ${pkg.name} requires Node >= ${required} in the Node ${major}.x branch (N-API >= ${pkg.cnoke.napi})`);
-    } else {
-        throw new Error(`This engine is based on Node ${process.versions.node}, but ${pkg.name} does not support the Node ${major}.x branch (N-API < ${pkg.cnoke.napi})`);
-    }
-}
-
-let arch = determine_arch();
-let triplet = `${process.platform}_${arch}`;
+let triplet = detect();
 
 let native = null;
 
@@ -61,62 +46,22 @@ try {
         case 'win32_ia32': { native = require('./build/koffi/win32_ia32/koffi.node'); } break;
         case 'win32_x64': { native = require('./build/koffi/win32_x64/koffi.node'); } break;
     }
-} catch (err) {
-    // Go on!
-}
+} catch {
+    // We don't try to detect musl distributions, just fallback to it if the GNU build failed
 
-// And now, search everywhere we know
-if (native == null) {
-    let roots = [__dirname];
-
-    if (process.resourcesPath != null)
-        roots.push(process.resourcesPath);
-
-    let names = [
-        `/build/koffi/${process.platform}_${arch}/koffi.node`,
-        `/koffi/${process.platform}_${arch}/koffi.node`,
-        `/node_modules/koffi/build/koffi/${process.platform}_${arch}/koffi.node`,
-        `/../../bin/Koffi/${process.platform}_${arch}/koffi.node`
-    ];
-
-    for (let root of roots) {
-        for (let name of names) {
-            let filename = root + name;
-
-            if (fs.existsSync(filename)) {
-                // Trick so that webpack does not try to do anything with require() call
-                native = eval('require')(filename);
-                break;
-            }
+    try {
+        switch (triplet) {
+            case 'linux_armhf': { native = require('./build/koffi/musl_armhf/koffi.node'); } break;
+            case 'linux_arm64': { native = require('./build/koffi/musl_arm64/koffi.node'); } break;
+            case 'linux_ia32': { native = require('./build/koffi/musl_ia32/koffi.node'); } break;
+            case 'linux_riscv64d': { native = require('./build/koffi/musl_riscv64d/koffi.node'); } break;
+            case 'linux_x64': { native = require('./build/koffi/musl_x64/koffi.node'); } break;
         }
-
-        if (native != null)
-            break;
+    } catch {
+        // Go on
     }
 }
 
-if (native == null)
-    throw new Error('Cannot find the native Koffi module; did you bundle it correctly?');
-if (native.version != pkg.version)
-    throw new Error('Mismatched native Koffi modules');
+let mod = init(triplet, native);
 
-module.exports = {
-    ...native,
-
-    // Deprecated functions
-    handle: util.deprecate(native.opaque, 'The koffi.handle() function was deprecated in Koffi 2.1, use koffi.opaque() instead', 'KOFFI001'),
-    callback: util.deprecate(native.proto, 'The koffi.callback() function was deprecated in Koffi 2.4, use koffi.proto() instead', 'KOFFI002')
-};
-
-let load = module.exports.load;
-
-module.exports.load = (...args) => {
-    let lib = load(...args);
-
-    lib.cdecl = util.deprecate((...args) => lib.func('__cdecl', ...args), 'The koffi.stdcall() function was deprecated in Koffi 2.7, use koffi.func(...) instead', 'KOFFI003');
-    lib.stdcall = util.deprecate((...args) => lib.func('__stdcall', ...args), 'The koffi.stdcall() function was deprecated in Koffi 2.7, use koffi.func("__stdcall", ...) instead', 'KOFFI004');
-    lib.fastcall = util.deprecate((...args) => lib.func('__fastcall', ...args), 'The koffi.fastcall() function was deprecated in Koffi 2.7, use koffi.func("__fastcall", ...) instead', 'KOFFI005');
-    lib.thiscall = util.deprecate((...args) => lib.func('__thiscall', ...args), 'The koffi.thiscall() function was deprecated in Koffi 2.7, use koffi.func("__thiscall", ...) instead', 'KOFFI006');
-
-    return lib;
-};
+module.exports = mod;
--- src/koffi/indirect.js
+++ src/koffi/indirect.js
@@ -21,79 +21,9 @@
 
 'use strict';
 
-const util = require('util');
-const fs = require('fs');
-const { get_napi_version, determine_arch } = require('../cnoke/src/tools.js');
-const pkg = require('./package.json');
+const { detect, init } = require('./src/init.js');
 
-if (process.versions.napi == null || process.versions.napi < pkg.cnoke.napi) {
-    let major = parseInt(process.versions.node, 10);
-    let required = get_napi_version(pkg.cnoke.napi, major);
+let triplet = detect();
+let mod = init(triplet, null);
 
-    if (required != null) {
-        throw new Error(`This engine is based on Node ${process.versions.node}, but ${pkg.name} requires Node >= ${required} in the Node ${major}.x branch (N-API >= ${pkg.cnoke.napi})`);
-    } else {
-        throw new Error(`This engine is based on Node ${process.versions.node}, but ${pkg.name} does not support the Node ${major}.x branch (N-API < ${pkg.cnoke.napi})`);
-    }
-}
-
-let arch = determine_arch();
-let triplet = `${process.platform}_${arch}`;
-
-let native = null;
-
-// Search everywhere we know
-{
-    let roots = [__dirname];
-
-    if (process.resourcesPath != null)
-        roots.push(process.resourcesPath);
-
-    let names = [
-        `/build/koffi/${process.platform}_${arch}/koffi.node`,
-        `/koffi/${process.platform}_${arch}/koffi.node`,
-        `/node_modules/koffi/build/koffi/${process.platform}_${arch}/koffi.node`,
-        `/../../bin/Koffi/${process.platform}_${arch}/koffi.node`
-    ];
-
-    for (let root of roots) {
-        for (let name of names) {
-            let filename = root + name;
-
-            if (fs.existsSync(filename)) {
-                // Trick so that webpack does not try to do anything with require() call
-                native = eval('require')(filename);
-                break;
-            }
-        }
-
-        if (native != null)
-            break;
-    }
-}
-
-if (native == null)
-    throw new Error('Cannot find the native Koffi module; did you bundle it correctly?');
-if (native.version != pkg.version)
-    throw new Error('Mismatched native Koffi modules');
-
-module.exports = {
-    ...native,
-
-    // Deprecated functions
-    handle: util.deprecate(native.opaque, 'The koffi.handle() function was deprecated in Koffi 2.1, use koffi.opaque() instead', 'KOFFI001'),
-    callback: util.deprecate(native.proto, 'The koffi.callback() function was deprecated in Koffi 2.4, use koffi.proto() instead', 'KOFFI002')
-};
-
-let load = module.exports.load;
-
-module.exports.load = (...args) => {
-    let lib = load(...args);
-
-    lib.cdecl = util.deprecate((...args) => lib.func('__cdecl', ...args), 'The koffi.stdcall() function was deprecated in Koffi 2.7, use koffi.func(...) instead', 'KOFFI003');
-    lib.stdcall = util.deprecate((...args) => lib.func('__stdcall', ...args), 'The koffi.stdcall() function was deprecated in Koffi 2.7, use koffi.func("__stdcall", ...) instead', 'KOFFI004');
-    lib.fastcall = util.deprecate((...args) => lib.func('__fastcall', ...args), 'The koffi.fastcall() function was deprecated in Koffi 2.7, use koffi.func("__fastcall", ...) instead', 'KOFFI005');
-    lib.thiscall = util.deprecate((...args) => lib.func('__thiscall', ...args), 'The koffi.thiscall() function was deprecated in Koffi 2.7, use koffi.func("__thiscall", ...) instead', 'KOFFI006');
-
-    return lib;
-};
+module.exports = mod;
